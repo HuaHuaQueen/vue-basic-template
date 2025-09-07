f50599897f389bbadafaa389fbeb9c6256f4a176
HuaHuaQueen <huahuaqueen12138999@gmail.com>
2025-09-08 00:26:30 +0800

    Update index.ts

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,10 +2,13 @@ import type { App } from 'vue'
 import { setupRouter } from '@/plugins/router'
 import { setupPinia } from '@/plugins/pinia'
 import { setupI18n } from '@/plugins/i18n'
-import { setupPrimevue } from '@/plugins/primevue'
+
+function setupProvide(app: App){
+  app.provide('APP_INFO', readonly(APP_INFO))
+}
 
 export const setupPlugins = (app: App) => {
-  setupPrimevue(app)
+setupProvide(app)
   setupRouter(app)
   setupPinia(app)
   setupI18n(app)
