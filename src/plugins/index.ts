23b164789c73b38334a4bf9d4cb27e3fcd280e5e
HuaHuaQueen <huahuaqueen12138999@gmail.com>
2026-05-01 01:24:36 +0800

    信息修正

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -3,12 +3,7 @@ import { setupRouter } from '@/plugins/router'
 import { setupPinia } from '@/plugins/pinia'
 import { setupI18n } from '@/plugins/i18n'
 
-function setupProvide(app: App){
-  app.provide('APP_INFO', readonly(APP_INFO))
-}
-
 export const setupPlugins = (app: App) => {
-setupProvide(app)
   setupRouter(app)
   setupPinia(app)
   setupI18n(app)
--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ const APP_INFO = {
   name,
   version,
   author,
-  description
+  description,
 }
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   const env: Record<string, string> = loadEnv(mode, process.cwd())
@@ -33,46 +33,48 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
         // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
         eslintrc: {
           enabled: true, // 是否自动生成 eslint 规则，建议生成之后设置 false
-          filepath: './.eslintrc-auto-import.json' // 指定自动导入函数 eslint 规则的文件
+          filepath: './.eslintrc-auto-import.json', // 指定自动导入函数 eslint 规则的文件
         },
-        dts: path.resolve(pathSrc, 'types', 'auto-imports.d.ts') // 指定自动导入函数TS类型声明文件路径
+        dts: path.resolve(pathSrc, 'types', 'auto-imports.d.ts'), // 指定自动导入函数TS类型声明文件路径
       }),
       Components({
         resolvers: [
           // 自动注册图标组件
           IconsResolver({
-            enabledCollections: [''] // 图标库 https://icon-sets.iconify.design/
-          })
+            enabledCollections: [''], // 图标库 https://icon-sets.iconify.design/
+          }),
         ],
-        dts: path.resolve(pathSrc, 'types', 'components.d.ts') // 指定自动导入组件TS类型声明文件路径
+        dts: path.resolve(pathSrc, 'types', 'components.d.ts'), // 指定自动导入组件TS类型声明文件路径
       }),
       Icons({
         // 自动安装图标库
-        autoInstall: true
+        autoInstall: true,
       }),
       createSvgIconsPlugin({
         // 指定需要缓存的图标文件夹
         iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
         // 指定symbolId格式
-        symbolId: 'icon-[dir]-[name]'
-      })
+        symbolId: 'icon-[dir]-[name]',
+      }),
     ],
     resolve: {
       alias: {
-        '@': pathSrc
-      }
+        '@': pathSrc,
+      },
     },
     server: {
       open: true, // 是否在服务器启动时自动打开浏览器
       port: Number(env.VITE_APP_PORT), // 服务器端口,
-      [env.VITE_APP_BASE_API]: {
-        target: env.VITE_APP_TARGET,
-        changeOrigin: true,
-        rewrite: (path: string) => path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`), '')
-      }
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_APP_TARGET,
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`), ''),
+        },
+      },
     },
     define: {
-      APP_INFO: JSON.stringify(APP_INFO)
-    }
+      __APP_INFO__: JSON.stringify(APP_INFO),
+    },
   }
 })
